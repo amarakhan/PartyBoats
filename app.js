033547ebbe7ef08d8f9462ef724bec9498e7af27
Amara Khan <amarakha@buffalo.edu>
2019-03-29 12:01:35 -0500

    changing port variable

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const mongoose = require("mongoose");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 mongoose.connect("mongodb://localhost/partyboatdb");
 
