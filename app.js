d067ec9f026b7da57ba9a611a5cf8a56e3c156b7
Amara Khan <amarakha@buffalo.edu>
2019-03-29 10:10:22 -0500

    the post form data writes to a partyboatdb collection

--- app.js
+++ app.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const mongoose = require("mongoose");
 const port = 3000;
+
+mongoose.connect("mongodb://localhost/partyboatdb");
 
 app.use(express.static("public"));
 app.use(express.static("public/images"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 
-var boats = [
-    {name: "party boat 1 - taxi boat", image: "/images/party1.jpg"},
-    {name: "Stena Line", image: "/images/suisse.jpg"},
-    {name: "Color Line", image: "/images/colorline.jpeg"}
-]
+var boatSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Boats = mongoose.model("Boats", boatSchema);
+var newBoats = mongoose.model("newBoats", boatSchema);
+// Boats.create({
+    // name: "Sea Imp",
+    // image: "/images/seaimp.jpg"
+    // name: "La Suisse", 
+    // image: "/images/suisse.jpg"
+    // name: "Color Line", 
+    // image: "/images/colorline.jpeg"
+    // name: "Taxi boat", 
+    // image: "/images/taxi.jpg"
+// }, function(err, boat){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("new boat added: ");
+//         console.log(boat);
+//     }
+// });
+
+
+// var boats = [
+//     {name: "Sea Imp", image: "/images/seaimp.jpg"},   
+//     {name: "Taxi boat", image: "/images/taxi.jpg"},
+//     {name: "La Suisse", image: "/images/suisse.jpg"},
+//     {name: "Color Line", image: "/images/colorline.jpeg"}
+// ]
 
 app.get("/",function(req,res){
     //res.render("home");
@@ -20,19 +50,37 @@ app.get("/",function(req,res){
 });
 
 app.get("/boats", function(req, res){
-
-    res.render("boats", {boats: boats});
+    Boats.find({}, function(err,allboats){
+        if(err){
+            console.log(err);
+        } else {
+            newBoats.find({}, function(err,allnewboats){
+                if(err){
+                    console.log(err);
+                } else {
+                    res.render("boats", {newboats: allnewboats, boats: allboats});
+                }
+            });
+            // res.render("boats", {boats: allboats});
+        }
+    });
 });
 
 app.post("/boats", function(req,res){
     var name = req.body.name;
     var image = req.body.image;
     var newBoat = {name: name, image: image}
-    boats.push(newBoat);
-    res.redirect("/boats");
+    // boats.push(newBoat);
+    newBoats.create(newBoat, function(err, newlyCreated){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/boats");
+        }
+    });
 });
 
-app.get("/boats/new", function(req,res){
+app.get("/new", function(req,res){
     res.render("new.ejs");
 });
 
